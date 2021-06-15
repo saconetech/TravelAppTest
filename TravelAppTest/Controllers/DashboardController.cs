b442bf315e077f0be9142293af212518bc958823
sacone <78349204+sacone@users.noreply.github.com>
2021-06-15 17:37:02 +0530

    Add project files.

---
+++ TravelAppTest/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelAppTest.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult JsonData()
+        {
+            StreamReader sr = new StreamReader(Server.MapPath("~/content/TravelData.json"));
+            var jsondata = Convert.ToString(sr.ReadToEnd());
+            //var jsondata = JsonConvert.DeserializeObject(sr.ReadToEnd());
+            return Json(jsondata, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    //internal class JsonConvert
+    //{
+    //    internal static object Deseializeobject(object p)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
